f25b1968851db8bfa8070752977d5de97b4a988f
chenhui-macbookpro2024 <CanoeByGuitar@gmail.com>
2025-04-01 17:48:16 +0800

    init

---
+++ meo/include/INode.h
@@ -0,0 +1,10 @@
+//
+// Created by ChenhuiWang on 2025/4/1.
+
+// Copyright (c) 2025 Mihoyo. All rights reserved.
+//
+
+#ifndef MEODINI_INODE_H
+#define MEODINI_INODE_H
+
+#endif //MEODINI_INODE_H
---
+++ meo/src/INode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ChenhuiWang on 2025/4/1.
+
+// Copyright (c) 2025 Mihoyo. All rights reserved.
+//
